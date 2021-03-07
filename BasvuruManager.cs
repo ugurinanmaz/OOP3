98adb280655124204c128d9ca0986eecbd3588dc
ugurinanmaz <ugurinanmaz@gmail.com>
2021-03-07 07:39:39 -0600

    Add project files.

---
+++ BasvuruManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        // method injection
+        public void BasvuruYap(IKrediManager krediManager, List<ILoggerService> loggerServices)
+        {
+            //Basvuran bilgilerini degerlendirme//
+            //
+            //bu sekilde yazarsak sadece konut kredisine gore heaplar 
+            //KonutKrediManager konutKrediManager = new KonutKrediManager();
+            //konutKrediManager.Hesapla();
+
+            krediManager.Hesapla();
+            foreach (var loggerService in loggerServices)
+            {
+                loggerService.Log();
+            }
+            
+            
+
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();   
+            }
+        }
+    }
+}
